84fbb40fbd92000bb49bf070956501ec92ed10ee
Pruthviraj Kachate <pruthvirajkachate2005@gmail.com>
2025-12-01 01:00:14 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import NavBar from './Components/NavBar/Navbar'
+import NavBar from './Components/NavBar/NavBar'
 import { Hero } from './Components/Hero/Hero'
 function App() {
 
